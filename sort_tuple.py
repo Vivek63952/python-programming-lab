17f47b8889720ff51036d50e415d97184ba32b08
Vivek63952 <106443442+Vivek63952@users.noreply.github.com>
2022-05-29 19:09:34 +0530

    sort tuple by key

---
+++ sort_tuple.py
@@ -0,0 +1,14 @@
+# Python code to sort a list of tuples according to given key.
+  
+def last(n):
+    return n[m]  
+   
+# function to sort the tuple   
+def sort(tuples):
+   
+    return sorted(tuples, key = last)
+    
+a = [(23, 45, 20), (25, 44, 39), (89, 40, 23)]
+m = 2
+print("Sorted:"),
+print(sort(a))
