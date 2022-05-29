fecfcaac3d2b4a8f3ec558860dc7ed0c0a4385dd
Vivek63952 <106443442+Vivek63952@users.noreply.github.com>
2022-05-29 23:04:22 +0530

    to iterate a dictionary in the python script

---
+++ dict_iterate.py
@@ -0,0 +1,6 @@
+using for loop to iterate a dictionary in the python script
+
+capitals = {"USA":"Washington D.C.", "France":"Paris", "India":"New Delhi"}
+
+for key in capitals:
+    print("Key = " + key + ", Value = " + capitals[key]) 
