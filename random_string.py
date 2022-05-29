b273928d402c42df6403db3c48b652881d9dc21a
Vivek63952 <106443442+Vivek63952@users.noreply.github.com>
2022-05-30 01:15:57 +0530

    to generate random string of length 5

---
+++ random_string.py
@@ -0,0 +1,10 @@
+#PROGRAM TO GENERATE RANDOM STRING OF LENGTH 5
+import random
+import string
+
+def randomString(stringLength):
+    """Generate a random string of 5 charcters"""
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+print ("Random String is ", randomString(5) )
