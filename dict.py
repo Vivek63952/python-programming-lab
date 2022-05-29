35128251a77e4dfb95cc5e916da98376c6821d7f
Vivek63952 <106443442+Vivek63952@users.noreply.github.com>
2022-05-29 22:12:11 +0530

    two list convert into a dictionary

---
+++ dict.py
@@ -0,0 +1,7 @@
+# PYTHON PROGRAM TO CONVERT TWO LIST INTO A DICTIONARY BY USING ZIP FUNC.
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+res_dict = dict(zip(keys, values))
+print(res_dict)
