dd3fa5cfeb5ec544e38f1daa4aecb036d89e6e3e
Vivek63952 <106443442+Vivek63952@users.noreply.github.com>
2022-05-29 23:48:42 +0530

    to find factorial using recursion

---
+++ recursion_fact.py
@@ -0,0 +1,16 @@
+#PROGRAM TO FIND FACTORIAL OF A NUMBER USING RECURSION
+
+def recur_fact(n): 
+   if n == 1:  
+       return n  
+   else:  
+       return n*recur_fact(n-1)  
+  
+num = int(input("Enter a number: "))  
+ 
+if num < 0:  
+   print("Sorry, factorial does not exist for negative numbers")  
+elif num == 0:  
+   print("The factorial of 0 is 1")  
+else:  
+   print("The factorial of",num,"is",recur_fact(num))  
