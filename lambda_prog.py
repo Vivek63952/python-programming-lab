1ea9680e3cb35be57217e06b77fa630695eeb6c4
Vivek63952 <106443442+Vivek63952@users.noreply.github.com>
2022-05-30 10:11:54 +0530

    remove non zero value from list

---
+++ lambda_prog.py
@@ -0,0 +1,10 @@
+#PYTHON PROGRAM TO REMOVE NONE VALUE FROM A GIVEN LIST USING LAMBDA FUNCTION
+def remove_none(nums):
+    result = filter(lambda v: v is not None, nums)
+    return list(result)
+
+nums = [12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
+print("Original list:")
+print(nums)
+print("\nRemove None value from the said list:")
+print(remove_none(nums))
